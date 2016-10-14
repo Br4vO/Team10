53e8b8d61aa23f91bed64ad047e6df2efb278915
seamane <eric_ctr_seaman247@yahoo.com>
2016-10-14 14:28:10 -0600

    player movement and animation added

--- SeriousGame/Assets/Scripts/PlayerHandler.cs
+++ SeriousGame/Assets/Scripts/PlayerHandler.cs
@@ -3,21 +3,48 @@ using System.Collections;
 
 public class PlayerHandler : MonoBehaviour {
     private Vector2 position;
+    private int walkingCount = 30;
+    float xOffset;
+    float yOffset;
+    Animation playerAnimation;
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         position = new Vector2(0, 0);
-	}
+        playerAnimation = GetComponent<Animation>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    if(walkingCount < 30)
+        {
+            ++walkingCount;
+            transform.Translate(new Vector3(xOffset, 0, yOffset) * 1 * Time.deltaTime);
+            if(walkingCount == 30)
+            {
+                playerAnimation.Play("Idle");
+            }
+        }
 	}
 
     public void MovePlayer(float xSwipeDirection, float ySwipeDirection)
     {
         position.x += xSwipeDirection;
         position.y += ySwipeDirection >= 0 ? ySwipeDirection : 0;//can only swipe up, not down
+
+        xOffset = xSwipeDirection;
+
+        if(ySwipeDirection < 0)
+        {
+            //swiped down, do nothing
+            yOffset = 0;
+        }
+        else
+        {
+            yOffset = ySwipeDirection;
+            playerAnimation.Play("Walk");
+            walkingCount = 0;
+        }
     }
 }
