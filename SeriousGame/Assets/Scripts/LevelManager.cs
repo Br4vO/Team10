abd18fa6639411deb32830585c944a0476b1dbc7
seamane <eric_ctr_seaman247@yahoo.com>
2016-10-18 15:53:14 -0600

    added to level manager

--- SeriousGame/Assets/Scripts/LevelManager.cs
+++ SeriousGame/Assets/Scripts/LevelManager.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class Food
 {
@@ -48,6 +48,9 @@ public class LevelManager : MonoBehaviour {
     private Food weekTotal;
     private Food dayTotal;
 
+    private int currLevel;
+    private List<Food> levelGoals;
+
     void Awake()
     {
         Application.targetFrameRate = 60;
@@ -59,6 +62,9 @@ public class LevelManager : MonoBehaviour {
         foodTotal = new Food();
         weekTotal = new Food();
         dayTotal = new Food();
+        currLevel = 1;
+
+        CreateLevelGoals();
     }
 	
 	// Update is called once per frame
@@ -66,4 +72,104 @@ public class LevelManager : MonoBehaviour {
 	{
 	
 	}
+
+    List<Food> GetLevelGoals()
+    {
+        return levelGoals;
+    }
+
+    int GetCurrentLevel()
+    {
+        return currLevel;
+    }
+
+    void CreateLevelGoals()
+    {
+        //level 1
+        Food temp = new Food();
+        temp.Apple = 3;
+        levelGoals.Add(temp);
+
+        //level 2
+        temp = new Food();
+        temp.Egg = 5;
+        levelGoals.Add(temp);
+
+        //level 3
+        temp = new Food();
+        temp.Broccoli = 2;
+        temp.Fish = 2;
+        levelGoals.Add(temp);
+
+        //level 4
+        temp = new Food();
+        temp.Banana = 3;
+        temp.Burger = 1;
+        levelGoals.Add(temp);
+
+        //level 5
+        temp = new Food();
+        temp.Broccoli = 2;
+        temp.Orange = 3;
+        temp.IceCream = 1;
+        levelGoals.Add(temp);
+
+        //level 6
+        temp = new Food();
+        temp.Apple = 2;
+        temp.Carrot = 2;
+        temp.FrenchFries = 1;
+        temp.Soda = 1;
+        levelGoals.Add(temp);
+
+        //level 7
+        temp = new Food();
+        temp.CandyBar = 1;
+        temp.Pizza = 1;
+        temp.Broccoli = 1;
+        temp.Banana = 1;
+        levelGoals.Add(temp);
+        
+        //level 8
+        temp = new Food();
+        temp.Orange = 1;
+        temp.Cheese = 1;
+        temp.Fish = 1;
+        temp.Carrot = 1;
+        temp.Soda = 1;
+        levelGoals.Add(temp);
+
+        //level 9
+        temp = new Food();
+        temp.IceCream = 1;
+        temp.Fish = 2;
+        temp.Egg = 1;
+        temp.Carrot = 1;
+        temp.Broccoli = 1;
+        temp.Orange = 1;
+        temp.Apple = 1;
+        levelGoals.Add(temp);
+
+        //level 10
+        temp = new Food();
+        temp.Egg = 3;
+        temp.Broccoli = 2;
+        temp.Fish = 2;
+        temp.Apple = 2;
+        temp.Banana = 1;
+        temp.Carrot = 1;
+        temp.Cheese = 1;
+        temp.Orange = 1;
+        levelGoals.Add(temp);
+
+        //level 11
+        temp = new Food();
+        temp.CandyBar = 1;
+        temp.FrenchFries = 1;
+        temp.Banana = 2;
+        temp.Egg = 2;
+        temp.Broccoli = 2;
+        temp.Cheese = 2;
+        levelGoals.Add(temp);
+    }
 }
